3afdb31bcc5b02940877fe0ec6bb8aae38eec2c5
Marli Frost <marli@frost.red>
2020-06-25 11:50:12 +0100

    fix: allow code to build

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,20 @@
-pub struct Error {}
+use core::{cmp, fmt, hash};
 
-impl fmt::Display for Error {}
-impl fmt::Debug for Error {}
+pub enum Error {}
+
+impl fmt::Display for Error {
+    fn fmt(&self, _: &mut fmt::Formatter) -> fmt::Result {
+        match *self {}
+    }
+}
+impl fmt::Debug for Error {
+    fn fmt(&self, _: &mut fmt::Formatter) -> fmt::Result {
+        match *self {}
+    }
+}
 
 #[cfg(feature = "std")]
 impl std::error::Error for Error {}
-
-type Result<T, E = Error> = core::result::Result<T, E>;
 
 /// Marker trait for types that a `Uri` can contain
 ///
@@ -17,72 +25,152 @@ type Result<T, E = Error> = core::result::Result<T, E>;
 pub unsafe trait StableStrRef: AsRef<str> {}
 
 unsafe impl<T: StableStrRef> StableStrRef for &'_ T {}
+
 unsafe impl StableStrRef for str {}
+
+#[cfg(feature = "std")]
 unsafe impl StableStrRef for String {}
 
-/// A Uri as defined by [RFC 3986]
-///
-///
-/// [RFC 3986]: https://tools.ietf.org/html/rfc3986
 // Represents the `URI-reference` ABNF rule
 pub struct Uri<T: StableStrRef> {
     data: T,
 }
 
 impl<T: StableStrRef> Uri<T> {
-    fn parse(data: T) -> Result<Self> {
-        unimplemented!()
+    pub fn parse(_: T) -> Result<Self, (T, Error)> {
+        todo!()
     }
 
     pub fn scheme(&self) -> Option<&str> {
-        unimplemented!()
+        todo!()
     }
     pub fn authority(&self) -> Option<&str> {
-        unimplemented!()
+        todo!()
     }
     pub fn path(&self) -> &str {
-        unimplemented!()
+        todo!()
     }
     pub fn query(&self) -> Option<&str> {
-        unimplemented!()
+        todo!()
     }
     pub fn fragment(&self) -> Option<&str> {
-        unimplemented!()
+        todo!()
     }
     
     // Components of Uri::authority
     pub fn userinfo(&self) -> Option<&str> {
-        unimplemented!()
+        todo!()
     }
     pub fn port(&self) -> Option<&str> {
-        unimplemented!()
+        todo!()
     }
     pub fn host(&self) -> Option<&str> {
-        unimplemented!()
+        todo!()
     }
     
-    fn into_inner(self) -> T {
+    pub fn into_inner(self) -> T {
         self.data
     }
 }
 
-impl<T: StableStrRef> fmt::Display for Uri<T> {} // Uri("https://github.com/Plecra")
-impl<T: StableStrRef> fmt::Debug for Uri<T> {} // Uri("https" : // "github.com" "/Plecra")
-impl<T: StableStrRef> AsRef<str> for Uri<T> {} // "https://github.com/Plecra"
+
+impl<T: StableStrRef> fmt::Display for Uri<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str(self.as_ref())
+    }
+}
+
+impl<T: StableStrRef> fmt::Debug for Uri<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("Uri(")?;
+        if let Some(scheme) = self.scheme() {
+            fmt::Debug::fmt(scheme, f)?;
+            f.write_str(" : ")?;
+        }
+        if self.authority().is_some() {
+            f.write_str("// ")?;
+            if let Some(userinfo) = self.userinfo() {
+                fmt::Debug::fmt(userinfo, f)?;
+                f.write_str(" @ ")?;
+            }
+            let host = self.host().unwrap();
+            fmt::Debug::fmt(host, f)?;
+            if let Some(port) = self.port() {
+                f.write_str(" : ")?;
+                fmt::Debug::fmt(port, f)?;
+            }
+            f.write_str(" ")?;
+        }
+        fmt::Debug::fmt(self.path(), f)?;
+        if let Some(query) = self.query() {
+            f.write_str(" ? ")?;
+            fmt::Debug::fmt(query, f)?;
+        }
+        if let Some(fragment) = self.fragment() {
+            f.write_str(" # ")?;
+            fmt::Debug::fmt(fragment, f)?;
+        }
+        f.write_str(")")?;
+        Ok(())
+    }
+}
+
+impl<T: StableStrRef> AsRef<str> for Uri<T> {
+    fn as_ref(&self) -> &str {
+        self.data.as_ref()
+    }
+}
 
 // Normalized comparisons
-impl<T: StableStrRef> PartialEq for Uri<T> {}
-impl<T: StableStrRef> Eq for Uri<T> {}
-impl<T: StableStrRef> PartialOrd for Uri<T> {}
-impl<T: StableStrRef> Ord for Uri<T> {}
-impl<T: StableStrRef> Hash for Uri<T> {}
+impl<T: StableStrRef> cmp::PartialEq for Uri<T> {
+    fn eq(&self, _: &Self) -> bool {
+        todo!()
+    }
+}
+impl<T: StableStrRef> cmp::Eq for Uri<T> {}
+impl<T: StableStrRef> cmp::PartialOrd for Uri<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl<T: StableStrRef> cmp::Ord for Uri<T> {
+    fn cmp(&self, _: &Self) -> cmp::Ordering {
+        todo!()
+    }
+}
+impl<T: StableStrRef> hash::Hash for Uri<T> {
+    fn hash<H: hash::Hasher>(&self, _: &mut H) {
+        todo!()
+    }
+}
 
-impl<T: StableStrRef + Clone> Clone for Uri<T> {}
-impl<T: StableStrRef + Sync> Sync for Uri<T> {}
-impl<T: StableStrRef + Send> Send for Uri<T> {}
-impl<T: StableStrRef + Default> Default for Uri<T> {}
+
+impl<T: StableStrRef + Clone> Clone for Uri<T> {
+    fn clone(&self) -> Self {
+        Self {
+            data: self.data.clone(),
+        }
+    }
+}
+unsafe impl<T: StableStrRef + Sync> Sync for Uri<T> {}
+unsafe impl<T: StableStrRef + Send> Send for Uri<T> {}
 
 #[cfg(feature = "serde")]
-impl<'de, T: StableStrRef> de::Deserialize<'de> for Uri<T> {}
-#[cfg(feature = "serde")]
-impl<T: StableStrRef> ser::Serialize for Uri<T> {}
+mod serde_impls {
+    use super::*;
+    use serde::*;
+    
+    impl<'de, T: StableStrRef + Deserialize<'de>> Deserialize<'de> for Uri<T> {
+        fn deserialize<D: de::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+            Uri::parse(T::deserialize(deserializer)?).map_err(|(s, _)| {
+                use de::Error;
+                D::Error::invalid_value(de::Unexpected::Str(s.as_ref()), &"a URI")
+            })
+        }
+    }
+    impl<T: StableStrRef + Serialize> Serialize for Uri<T> {
+        fn serialize<S: ser::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+            self.data.serialize(serializer)
+        }
+    }
+}
